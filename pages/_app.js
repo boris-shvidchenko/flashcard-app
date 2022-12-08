fc534003dc10f927173b565d2caa6eb5c1df250d
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2022-12-08 13:50:57 -0800

    Added files for initial repo setup for new project.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+// Components
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// Styles
+import '../styles/globals.css';
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta charSet='utf-8' />
+        <meta name="description" content="Flashcard app" />
+        <meta name="keywords" content="Boris Shvidchenko, Portfolio, Flashcards, App" />
+        <meta name="author" content="Boris Shvidchenko" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Flashcards</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
-
-export default MyApp
