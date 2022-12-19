35c4307cbe20b794c5d59efda4d7df061f5b9b1a
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2022-12-19 07:40:41 -0800

    Styled Loading component and added spinner

--- components/Loading.js
+++ components/Loading.js
@@ -1,7 +1,20 @@
+import { Oval } from 'react-loader-spinner';
+
 export default function Loading() {
     return (
         <div className='bg-black/90 absolute w-full h-full z-50'>
-            <h1 className='text-white text-5xl w-40 mx-auto mt-40'>Loading...</h1>
+            <div className=' w-40 mx-auto mt-40'>
+                <Oval
+                    height={80}
+                    width={80}
+                    color="#f7f5f5"
+                    visible={true}
+                    ariaLabel='oval-loading'
+                    secondaryColor="#f7f5f5"
+                    strokeWidth={2}
+                    strokeWidthSecondary={2}
+                />
+            </div>
         </div>
     )
 }
