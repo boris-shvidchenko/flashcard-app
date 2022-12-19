396c988e137a06a6f7c995df8f54055f44685836
BORIS SHVIDCHENKO <bshv2020@gmail.com>
2022-12-19 10:05:56 -0800

    update

--- pages/_document.js
+++ pages/_document.js
@@ -6,11 +6,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        
+
         {/* The link tags below are for/from Google Fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Merriweather&display=swap" rel="stylesheet" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
